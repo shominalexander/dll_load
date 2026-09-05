79194e426b48c0eee30f0792cfb4cc6b541154c8
shominalexander <40673785+shominalexander@users.noreply.github.com>
2026-09-05 21:00:14 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,9 @@
 fn main() {
  let all: Vec<String> = std::env::args().collect();
 
- let dll: &String  = &all[1];
+ let one: &String  = &all[1];
 
- unsafe {
-  match libloading::Library::new(dll) {
-   Ok(library) => {
-    println!("library: {:?}", library);
-
-   }//Ok(library) => {
-
-   Err(error) => {
-    println!("error: {:?}", error);
-
-   }//Err(error) => {
-  }//match libloading::Library::new("") {
- }//unsafe {
-
- match std::fs::read(dll) {
+ match std::fs::read(one) {
   Ok(slice_u8) => {
    match goblin::pe::PE::parse(&slice_u8) {
     Ok(pe) => {
@@ -28,6 +14,40 @@ fn main() {
      println!("pe.libraries:"); for library in pe.libraries { println!("{:?}", library); }
 
      println!("pe.exports:"); for export in pe.exports { println!("{:?}", export); }
+
+     unsafe {
+      match libloading::Library::new(one) {
+       Ok(library) => {
+        println!("library: {:?}", library);
+
+        match library.get(b"local_ip_address\0") {
+         Ok(local_ip_address) => {
+          let f_local_ip_address: libloading::Symbol<extern "C" fn() -> *const std::os::raw::c_char> = local_ip_address;
+
+          let c_local_ip_address: *const std::os::raw::c_char = f_local_ip_address();
+
+          if c_local_ip_address.is_null() {
+           println!("local ip address is null"); 
+
+          } else {//if c_local_ip_address.is_null() {
+           println!("local ip address: {:?}", std::ffi::CStr::from_ptr(c_local_ip_address).to_string_lossy().into_owned()); 
+
+          }//} else {//if c_local_ip_address.is_null() {
+         }//Ok(local_ip_address) {
+
+         Err(error) => {
+          println!("error: {:?}", error);
+
+         }//Err(error) => {
+        }//match library.get(b"local_ip_address\0") {
+       }//Ok(library) => {
+
+       Err(error) => {
+        println!("error: {:?}", error);
+
+       }//Err(error) => {
+      }//match libloading::Library::new(one) {
+     }//unsafe {
     }//Ok(pe) => {
 
     Err(error) => {
@@ -41,5 +61,6 @@ fn main() {
    println!("error: {:?}", error);
 
   }//Err(error) => {
- };//match std::fs::read(dll) {
+ };//match std::fs::read(one) {
 }//fn main() {
+
